ef5a1bc5f3dae271c646c8f3be9586c8f85b6139
Grégoire de Massé <demasse.gregoire@yahoo.fr>
2020-07-27 00:54:19 +0200

    Add debug macro + better write

--- src/procedures.rs
+++ src/procedures.rs
@@ -2,6 +2,7 @@ use super::interpreter::{Exp, Env, Error};
 
 use std::rc::Rc;
 
+// TODO: Some macro like #[proc(name, ?lib)] to generate a list of all the proc exported for a lib
 pub fn equal(expression: Vec<Exp>, _environment: Rc<Env>) -> Result<Exp, Error> {
     Ok(Exp::Bool(expression[0] == expression[1]))
 }
@@ -12,7 +13,6 @@ pub fn add(expression: Vec<Exp>, _environment: Rc<Env>) -> Result<Exp, Error> {
         [Exp::Float(a), Exp::Float(b)] => Ok(Exp::Float(a + b)),
         _ => Err(Error::SyntaxError("+ expects 2 arguments".to_string(), None))
     }
-    
 }
 
 pub fn mul(expression: Vec<Exp>, _environment: Rc<Env>) -> Result<Exp, Error> {
@@ -25,13 +25,13 @@ pub fn mul(expression: Vec<Exp>, _environment: Rc<Env>) -> Result<Exp, Error> {
 
 // rewrite with the port thing and stdout
 pub fn write(expression: Vec<Exp>, _environment: Rc<Env>) -> Result<Exp, Error> {
-    if expression.len() < 1 {
-        print!("{:?}", expression[0]);
-        Ok(Exp::List(vec![]))
-    } else {
-        print!("{:?}", expression);
-        Ok(Exp::List(vec![]))
-    }
+    print!("{}", expression[0]);
+    Ok(Exp::Bool(true))
+}
+
+pub fn dbg(expression: Vec<Exp>, _environment: Rc<Env>) -> Result<Exp, Error> {
+    println!("{:?}", expression);
+    Ok(Exp::Bool(true))
 }
 
 pub fn gt(expression: Vec<Exp>, _environment: Rc<Env>) -> Result<Exp, Error> {
